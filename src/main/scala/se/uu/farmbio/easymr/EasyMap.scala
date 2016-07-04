93118aa4bd79b1f4f5e944beb04ee9dccddd7037
marco <marco@osboxes>
2016-07-04 15:01:11 +0200

    Map support for whole files

--- src/main/scala/se/uu/farmbio/easymr/EasyMap.scala
+++ src/main/scala/se/uu/farmbio/easymr/EasyMap.scala
@@ -2,18 +2,34 @@ package se.uu.farmbio.easymr
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.HashPartitioner
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
+import org.apache.hadoop.io.NullWritable
+import java.io.File
+import org.apache.commons.io.FilenameUtils
+
+class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
+  override def generateActualKey(key: Any, value: Any): Any =
+    NullWritable.get()
+  override def generateFileNameForKeyValue(
+    key: Any,
+    value: Any,
+    name: String): String =
+    key.asInstanceOf[String]
+}
 
 case class EasyMapParams(
-    command: String = null,
-    trimComandOutput: Boolean = true,
-    imageName: String = "ubuntu:14.04",
-    inputPath: String = null,
-    outputPath: String = null,
-    fifoReadTimeout: Int = RunUtils.FIFO_READ_TIMEOUT,
-    local: Boolean = false)
+  command: String = null,
+  trimComandOutput: Boolean = true,
+  imageName: String = "ubuntu:14.04",
+  inputPath: String = null,
+  outputPath: String = null,
+  fifoReadTimeout: Int = RunUtils.FIFO_READ_TIMEOUT,
+  wholeFiles: Boolean = false,
+  local: Boolean = false)
 
 object EasyMap {
-  
+
   def run(params: EasyMapParams) = {
 
     //Start Spark context
@@ -23,44 +39,84 @@ object EasyMap {
       conf.setMaster("local[*]")
     }
     val sc = new SparkContext(conf)
-    
+
     //Read input data
-    val data = sc.textFile(params.inputPath)
-    
+    val data = readInputData(
+      sc,
+      params.inputPath,
+      params.outputPath,
+      params.wholeFiles)
+
     //Format command
     val toRun = params.command
       .replaceAll("<input>", "/inputFifo")
       .replaceAll("<output>", "/outputFifo")
 
     //Map data
-    val result = data.map { record =>
-      //Make fifos
-      val inputFifo = RunUtils.mkfifo("input")
-      val outputFifo = RunUtils.mkfifo("output")
-      //Write record to fifo
-      RunUtils.writeToFifo(inputFifo, record)
-      //Run command in container
-      val dockerOpts = s"-v ${inputFifo.getAbsolutePath}:/inputFifo " +
-        s"-v ${outputFifo.getAbsolutePath}:/outputFifo"
-      RunUtils.dockerRun(toRun, params.imageName, dockerOpts)
-      //Read result from fifo
-      val results = RunUtils.readFromFifo(outputFifo, params.fifoReadTimeout)
-      //Delete the fifos
-      inputFifo.delete
-      outputFifo.delete
-      //Trim results and return
-      if(params.trimComandOutput) {
-        results.trim
-      } else {
-        results 
-      }
+    val result = data.map {
+      case (index, record) =>
+        //Make fifos
+        val inputFifo = RunUtils.mkfifo("input")
+        val outputFifo = RunUtils.mkfifo("output")
+        //Write record to fifo
+        RunUtils.writeToFifo(inputFifo, record)
+        //Run command in container
+        val dockerOpts = s"-v ${inputFifo.getAbsolutePath}:/inputFifo " +
+          s"-v ${outputFifo.getAbsolutePath}:/outputFifo"
+        RunUtils.dockerRun(toRun, params.imageName, dockerOpts)
+        //Read result from fifo
+        val results = RunUtils.readFromFifo(outputFifo, params.fifoReadTimeout)
+        //Delete the fifos
+        inputFifo.delete
+        outputFifo.delete
+        //Trim results and return
+        if (params.trimComandOutput) {
+          (index, results.trim)
+        } else {
+          (index, results)
+        }
     }
-    
-    result.saveAsTextFile(params.outputPath)
-    
+
+    //Save results
+    if (params.wholeFiles) { //Save with multiple output
+      val numFiles = result.map(_._1).distinct.count
+      result.partitionBy(new HashPartitioner(numFiles.toInt))
+        .saveAsHadoopFile(params.outputPath,
+          classOf[String],
+          classOf[String],
+          classOf[RDDMultipleTextOutputFormat])
+    } else { //Save as single file
+      result.map(_._2) //remove index
+        .saveAsTextFile(params.outputPath)
+    }
+
     //Stop Spark context
     sc.stop
 
+  }
+
+  private[easymr] def readInputData(
+    sc: SparkContext,
+    inputPath: String,
+    outputPath: String,
+    wholeFiles: Boolean) = {
+    if (wholeFiles) {
+      //Get output extension
+      val outExt = FilenameUtils.getExtension(outputPath)
+      //Load files
+      sc.wholeTextFiles(inputPath)
+        .map {
+          case (filename, content) =>
+            //Trim extension and path
+            val noExt = FilenameUtils.removeExtension(filename)
+            val trimmedName = FilenameUtils.getBaseName(noExt)
+            //Set trimmed name and index, with output extension
+            (s"${trimmedName}.${outExt}", content)
+        }
+    } else {
+      sc.textFile(inputPath)
+        .map((inputPath, _))
+    }
   }
 
 }
--- src/test/scala/se/uu/farmbio/easymr/EasyMapReduceTest.scala
+++ src/test/scala/se/uu/farmbio/easymr/EasyMapReduceTest.scala
@@ -13,7 +13,8 @@ import java.io.PrintWriter
 import org.scalatest.BeforeAndAfterAll
 import scala.reflect.io.Path
 import org.apache.commons.io.FileUtils
-
+import org.apache.commons.io.FilenameUtils
+import java.io.File
 
 @RunWith(classOf[JUnitRunner])
 class EasyMapReduceTest extends FunSuite with BeforeAndAfterAll {
@@ -30,12 +31,12 @@ class EasyMapReduceTest extends FunSuite with BeforeAndAfterAll {
       command = "rev <input> > <output>",
       imageName = "ubuntu:14.04",
       local = true,
-      inputPath = getClass.getResource("dna.txt").getPath,
+      inputPath = getClass.getResource("dna/dna.txt").getPath,
       outputPath = tempDir.getAbsolutePath + "/rev.txt",
       fifoReadTimeout = 30)
     EasyMap.run(params)
 
-    val reverseTest = Source.fromFile(getClass.getResource("dna.txt").getPath)
+    val reverseTest = Source.fromFile(getClass.getResource("dna/dna.txt").getPath)
       .getLines.map(_.reverse)
 
     val sc = new SparkContext(conf)
@@ -47,15 +48,17 @@ class EasyMapReduceTest extends FunSuite with BeforeAndAfterAll {
   }
 
   test("easy reduce") {
-    
+
     //Make a test input from DNA
-    val lineCount = Source.fromFile(getClass.getResource("dna.txt").getPath).getLines
-      .map(_.filter(n => n == 'g' || n == 'c').length)    
-    new PrintWriter(tempDir.getAbsolutePath + "/count_by_line.txt") { 
+    val lineCount = Source.fromFile(
+      getClass.getResource("dna/dna.txt").getPath)
+      .getLines
+      .map(_.filter(n => n == 'g' || n == 'c').length)
+    new PrintWriter(tempDir.getAbsolutePath + "/count_by_line.txt") {
       write(lineCount.mkString("\n"))
-      close 
+      close
     }
-    
+
     val params = EasyReduceParams(
       command = "expr $(cat <input:1>) + $(cat <input:2>) > <output>",
       imageName = "ubuntu:14.04",
@@ -64,18 +67,43 @@ class EasyMapReduceTest extends FunSuite with BeforeAndAfterAll {
       outputPath = tempDir.getAbsolutePath + "/sum.txt",
       fifoReadTimeout = 30)
     EasyReduce.run(params)
-    
+
     val sc = new SparkContext(conf)
     val sumOut = sc.textFile(tempDir.getAbsolutePath + "/sum.txt").first
-    sc.stop 
-    
+    sc.stop
+
     val sumTest = Source.fromFile(tempDir.getAbsolutePath + "/count_by_line.txt")
       .getLines.map(_.toInt).sum
-    
+
     assert(sumOut.toInt == sumTest)
-    
+
   }
-  
+
+  test("easy map, multiple inputs") {
+
+    val params = EasyMapParams(
+      command = "rev <input> > <output>",
+      imageName = "ubuntu:14.04",
+      local = true,
+      inputPath = getClass.getResource("dna").getPath,
+      outputPath = tempDir.getAbsolutePath + "/seq.rev",
+      wholeFiles = true,
+      fifoReadTimeout = 30)
+    EasyMap.run(params)
+
+    Seq("dna.txt", "dna1.txt", "dna2.txt").foreach { input =>
+      val reverseTest = Source.fromFile(
+          getClass.getResource("dna/" + input).getPath)
+          .getLines.map(_.reverse)
+      val outPath = tempDir.getAbsolutePath + s"/seq.rev/$input" 
+      val reverseOut = Source.fromFile(
+          FilenameUtils.removeExtension(outPath) + ".rev")
+          .getLines
+      assert(reverseTest.toSet == reverseOut.toSet)
+    }
+
+  }
+
   override def afterAll {
     FileUtils.deleteDirectory(tempDir)
   }
