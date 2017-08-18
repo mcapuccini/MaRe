0e2ae72a5037103b085dd375aac7d20ac8d22991
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-08-18 15:56:32 +0200

    Add test for simple map reduce

--- src/test/scala/se/uu/it/easymr/GcCountTest.scala
+++ src/test/scala/se/uu/it/easymr/GcCountTest.scala
@@ -57,5 +57,29 @@ class GcCountTest extends FunSuite with SharedSparkContext {
     assert(res == toMatch + "\n")
 
   }
+  
+  // This is inefficient for this use case, but it is important to test
+  test("GC count in DNA string, record by record") {
+    
+    val rdd = sc.textFile(getClass.getResource("dna/dna.txt").getPath)
+
+    val res = new EasyMapReduce(rdd)
+      .map(
+        imageName = "ubuntu:xenial",
+        command = "grep -o '[gc]' /input | wc -l > /output")
+      .reduce(
+        imageName = "ubuntu:xenial",
+        command = "cat /input1 /input2 | awk '{s+=$1} END {print s}' > /output")
+
+    // Check if results matches with the one computed with the standard RDD API
+    val toMatch = sc.textFile(getClass.getResource("dna/dna.txt").getPath)
+      .map(_.count(c => c == 'g' || c == 'c').toString)
+      .reduce {
+        case (lineCount1, lineCount2) =>
+          (lineCount1.toInt + lineCount2.toInt).toString
+      }
+    assert(res == toMatch + "\n")
+
+  }
 
 }
