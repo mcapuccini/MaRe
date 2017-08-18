72d16f98174e3024321c03b279a32f0ba82665b9
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-08-18 14:24:01 +0200

    Add simple map and reduce

--- src/main/scala/se/uu/it/easymr/EasyMapReduce.scala
+++ src/main/scala/se/uu/it/easymr/EasyMapReduce.scala
@@ -46,6 +46,51 @@ private[easymr] object EasyMapReduce {
     output
 
   }
+  
+  def reduceLambda(
+    imageName: String,
+    command: String,
+    reduceInputMountPoint1: String,
+    reduceInputMountPoint2: String,
+    outputMountPoint: String,
+    record1: String,
+    record2: String,
+    recordDelimiter: String) = {
+
+    // Create temporary files
+    val inputFile1 = EasyFiles.writeToTmpFile(Seq(record1).iterator, recordDelimiter)
+    val inputFile2 = EasyFiles.writeToTmpFile(Seq(record2).iterator, recordDelimiter)
+    val outputFile = EasyFiles.createTmpFile
+
+    // Run docker
+    val docker = new EasyDocker
+    docker.run(
+      imageName,
+      command,
+      bindFiles = Seq(inputFile1, inputFile2, outputFile),
+      volumeFiles = Seq(
+          new File(reduceInputMountPoint1),
+          new File(reduceInputMountPoint2),
+          new File(outputMountPoint)))
+
+    // Retrieve output
+    val output = EasyFiles.readFromFile(outputFile, recordDelimiter)
+
+    // Remove temporary files
+    log.info(s"Deleteing temporary file: ${inputFile1.getAbsolutePath}")
+    inputFile1.delete
+    log.info(s"Temporary file '${inputFile1.getAbsolutePath}' deleted successfully")
+    log.info(s"Deleteing temporary file: ${inputFile2.getAbsolutePath}")
+    inputFile2.delete
+    log.info(s"Temporary file '${inputFile2.getAbsolutePath}' deleted successfully")
+    log.info(s"Deleteing temporary file: ${outputFile.getAbsolutePath}")
+    outputFile.delete
+    log.info(s"Temporary file '${outputFile.getAbsolutePath}' deleted successfully")
+
+    // Return output
+    output
+
+  }
 
 }
 
@@ -65,7 +110,9 @@ private[easymr] object EasyMapReduce {
 class EasyMapReduce(
     private val rdd: RDD[String],
     val inputMountPoint: String = "/input",
-    val outputMountPoint: String = "/output") extends Serializable {
+    val outputMountPoint: String = "/output",
+    val reduceInputMountPoint1: String = "/input1",
+    val reduceInputMountPoint2: String = "/input2") extends Serializable {
   
   // Logger
   @transient private lazy val log = Logger.getLogger(getClass.getName)
@@ -85,7 +132,12 @@ class EasyMapReduce(
    * @param inputMountPoint mount point for the input chunk that is passed to the containers
    */
   def setInputMountPoint(inputMountPoint: String) = {
-    new EasyMapReduce(rdd, inputMountPoint, outputMountPoint)
+    new EasyMapReduce(
+        rdd, 
+        inputMountPoint, 
+        outputMountPoint, 
+        reduceInputMountPoint1, 
+        reduceInputMountPoint2)
   }
 
   /**
@@ -94,7 +146,42 @@ class EasyMapReduce(
    * @param outputMountPoint mount point where the processed data is read back to Spark
    */
   def setOutputMountPoint(outputMountPoint: String) = {
-    new EasyMapReduce(rdd, inputMountPoint, outputMountPoint)
+    new EasyMapReduce(
+        rdd, 
+        inputMountPoint, 
+        outputMountPoint, 
+        reduceInputMountPoint1, 
+        reduceInputMountPoint2)
+  }
+  
+  /**
+   * It sets the mount point for the first input file that is passed to the containers in the reduce method.
+   *
+   * @param reduceInputMountPoint1 mount point for the first input file that is passed to the containers 
+   * in the reduce method
+   */
+  def setReduceInputMountPoint1(reduceInputMountPoint1: String) = {
+    new EasyMapReduce(
+        rdd, 
+        inputMountPoint, 
+        outputMountPoint, 
+        reduceInputMountPoint1, 
+        reduceInputMountPoint2)
+  }
+  
+  /**
+   * It sets the mount point for the second input file that is passed to the containers in the reduce method.
+   *
+   * @param reduceInputMountPoint2 mount point for the second input file that is passed to the containers 
+   * in the reduce method
+   */
+  def setReduceInputMountPoint2(reduceInputMountPoint2: String) = {
+    new EasyMapReduce(
+        rdd, 
+        inputMountPoint, 
+        outputMountPoint, 
+        reduceInputMountPoint1, 
+        reduceInputMountPoint2)
   }
 
   /**
@@ -150,6 +237,62 @@ class EasyMapReduce(
           imageName, command,
           inputMountPoint, outputMountPoint,
           records.iterator, recordDelimiter)
+          .map(_ + recordDelimiter)
+          .mkString
+    }
+
+  }
+  
+  /**
+   * It maps each RDD record through a Docker container command.
+   * Data is mounted to the specified inputMountPoint and read back
+   * from the specified outputMountPoint.
+   *
+   * @param imageName a Docker image name available in each node
+   * @param command a command to run in the Docker container, this should read from
+   * inputMountPoint and write back to outputMountPoint
+   */
+  def map(
+    imageName: String,
+    command: String) = {
+
+    val resRDD = rdd.map { record =>
+      val resIt = EasyMapReduce.mapLambda(
+        imageName, command,
+        inputMountPoint, outputMountPoint,
+        Seq(record).iterator, recordDelimiter)
+      resIt.next
+    }
+    new EasyMapReduce(resRDD, inputMountPoint, outputMountPoint)
+
+  }
+  
+  /**
+   * It reduces a RDD to a single String using a Docker container command. 
+   * The command is applied to couples of RDD records. Data is mounted to the specified
+   * reduceInputMountPoint1 and reduceInputMountPoint2, and it is read back from 
+   * the specified outputMountPoint.
+   *
+   * @param imageName a Docker image name available in each node
+   * @param command a command to run in the Docker container, this should read from
+   * reduceInputMountPoint1 and reduceInputMountPoint2, and it should write back to 
+   * outputMountPoint. The command should perform an associative and commutative operation 
+   * (for the parallelization to work).
+   *
+   */
+  def reduce(
+    imageName: String,
+    command: String) = {
+
+    // Reduce
+    rdd.reduce {
+      case (r1, r2) =>
+        EasyMapReduce.reduceLambda(
+          imageName, command,
+          reduceInputMountPoint1, 
+          reduceInputMountPoint2, 
+          outputMountPoint,
+          r1, r2, recordDelimiter)
           .map(_ + recordDelimiter)
           .mkString
     }
