b490fde7e255708684236f56b142d65ed05c6d13
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-10-19 14:24:26 +0200

    uncomment first test

--- src/test/scala/se/uu/it/easymr/CpSignTest.scala
+++ src/test/scala/se/uu/it/easymr/CpSignTest.scala
@@ -19,45 +19,45 @@ import sun.misc.BASE64Decoder
 @RunWith(classOf[JUnitRunner])
 class CpSignTest extends FunSuite with SharedSparkContext {
 
-  //  test("Train ACP in parallel with CPSign") {
-  //
-  //    val rdd = sc.parallelize(1 to 3).map(_.toString)
-  //
-  //    val res = new EasyMapReduce(rdd)
-  //      .setOutputMountPoint("/out.txt")
-  //      .setReduceInputMountPoint1("/model1.txt")
-  //      .setReduceInputMountPoint2("/model2.txt")
-  //      .map(
-  //        imageName = "mcapuccini/cpsign",
-  //        command = "java -jar cpsign-0.6.1.jar train " +
-  //          "-t data_small_train.sdf " +
-  //          "-mn out " +
-  //          "-mo /tmp.cpsign " +
-  //          "-c 1 " +
-  //          "--labels 0 1 " +
-  //          "-rn class " +
-  //          "--license cpsign0.6-standard.license && " +
-  //          "[ -e tmp.cpsign ] && " + // workaround for cpsign bug (it always exits with 0)
-  //          "base64 < /tmp.cpsign | tr -d '\n' > /out.txt")
-  //      .reduce(
-  //        imageName = "mcapuccini/cpsign",
-  //        command =
-  //          "base64 -d < /model1.txt > /model1.cpsign && " +
-  //            "base64 -d < /model2.txt > /model2.cpsign && " +
-  //            "java -jar cpsign-0.6.1.jar fast-aggregate " +
-  //            "-m /model1.cpsign /model2.cpsign " +
-  //            "-mo /tmp.cpsign " +
-  //            "--license cpsign0.6-standard.license && " +
-  //            "[ -e tmp.cpsign ] && " + // workaround for cpsign bug (it always exits with 0)
-  //            "base64 < /tmp.cpsign | tr -d '\n' > /out.txt")
-  //
-  //    // Test that we get a cpsign Jar archive as result
-  //    val base64 = new BASE64Decoder()
-  //    val jarBytes = base64.decodeBuffer(res)
-  //    val jar = new JarInputStream(new ByteArrayInputStream(jarBytes))
-  //    assert(Option(jar.getManifest).isDefined)
-  //
-  //  }
+  test("Train ACP in parallel with CPSign") {
+
+    val rdd = sc.parallelize(1 to 3).map(_.toString)
+
+    val res = new EasyMapReduce(rdd)
+      .setOutputMountPoint("/out.txt")
+      .setReduceInputMountPoint1("/model1.txt")
+      .setReduceInputMountPoint2("/model2.txt")
+      .map(
+        imageName = "mcapuccini/cpsign",
+        command = "java -jar cpsign-0.6.1.jar train " +
+          "-t data_small_train.sdf " +
+          "-mn out " +
+          "-mo /tmp.cpsign " +
+          "-c 1 " +
+          "--labels 0 1 " +
+          "-rn class " +
+          "--license cpsign0.6-standard.license && " +
+          "[ -e tmp.cpsign ] && " + // workaround for cpsign bug (it always exits with 0)
+          "base64 < /tmp.cpsign | tr -d '\n' > /out.txt")
+      .reduce(
+        imageName = "mcapuccini/cpsign",
+        command =
+          "base64 -d < /model1.txt > /model1.cpsign && " +
+            "base64 -d < /model2.txt > /model2.cpsign && " +
+            "java -jar cpsign-0.6.1.jar fast-aggregate " +
+            "-m /model1.cpsign /model2.cpsign " +
+            "-mo /tmp.cpsign " +
+            "--license cpsign0.6-standard.license && " +
+            "[ -e tmp.cpsign ] && " + // workaround for cpsign bug (it always exits with 0)
+            "base64 < /tmp.cpsign | tr -d '\n' > /out.txt")
+
+    // Test that we get a cpsign Jar archive as result
+    val base64 = new BASE64Decoder()
+    val jarBytes = base64.decodeBuffer(res)
+    val jar = new JarInputStream(new ByteArrayInputStream(jarBytes))
+    assert(Option(jar.getManifest).isDefined)
+
+  }
 
   test("Train ACP in parallel with CPSign, aggregate predictions only") {
 
